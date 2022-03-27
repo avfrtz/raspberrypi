aedbc9bb5012d6b6197b7001dc318ec6f87dbe34
avfrtz <alfritz1422@gmail.com>
2022-03-27 02:40:16 -0400

    q1

--- Assembly/Module5/Hw5/celToFah.s
+++ Assembly/Module5/Hw5/celToFah.s
@@ -8,17 +8,19 @@ main:
     LDR r0, =prompt
     BL printf
     LDR r0, =formatString
-    LDR r1, =tempFah
+    LDR r1, =tempCel
     BL scanf
 
-    LDR r0, =tempFah
+
+    LDR r0, =tempCel
     LDR r0, [r0]
-    MOV r1, #32
-    SUB r0, r0, r1
-    MOV r1, #5
-    MUL r0, r0, r1
     MOV r1, #9
+    MUL r0, r0, r1
+    MOV r1, #5
     BL __aeabi_idiv // this function divides r1/r0 and stores it in r0
+
+    MOV r1, #32
+    ADD r0, r0, r1
 
     MOV r1, r0
     LDR r0, =output
@@ -29,7 +31,7 @@ main:
     MOV pc, lr
 
 .data
-    output: .asciz "\nThe temperature in Celsius is %d\n"
-    prompt: .asciz "Enter temperature in Fahrenheit: "
-    tempFah: .word 0
+    output: .asciz "\nThe temperature in Fahrenheit is %d\n"
+    prompt: .asciz "Enter temperature in Celsius: "
+    tempCel: .word 0
     formatString: .asciz "%d"
